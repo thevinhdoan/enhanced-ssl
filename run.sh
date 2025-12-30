ab50f6d03739c703ad081c14a0a14405a4071b44
thevinhdoan <thevinhdoan03@gmail.com>
2025-12-30 17:28:25 +0000

    feat: prepare separate scripts for adaptformer & lora

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-python3 train.py \
-  --c config/lora/pet-ensembled/dtd/3-shot/dinov2/config.yaml \
-  --lambda_1 0.01 \
-  --lambda_2 0.05 \
-  --grouping_update_interval 2 \
-  --uratio 4
---
+++ run_lora.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export CUDA_VISIBLE_DEVICES=0
+python3 train.py \
+  --c config/lora/pet-ensembled/dtd/3-shot/dinov2/config.yaml \
+  --lambda_1 0.0 \
+  --lambda_2 0.0 \
+  --grouping_update_interval 2 \
+  --uratio 12
+python3 train.py \
+  --c config/lora/pet-ensembled-across-nets/dtd/3-shot/dinov2/config.yaml \
+  --lambda_1 0.0 \
+  --lambda_2 0.0 \
+  --grouping_update_interval 2 \
+  --uratio 12
