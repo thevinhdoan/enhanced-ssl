aa5db92713d6d6180ba787eb7952d5706a063102
vinhdoanthe03 <vinh.doanthe@pixta.co.jp>
2026-01-22 20:58:01 +0000

    feat: add standalone script run_infer.sh

---
+++ run_infer.sh
@@ -0,0 +1,7 @@
+python3 infer.py \
+    --checkpoint ... \
+    --dataset dtd \
+    --num_classes 47 \
+    --num_labels 141 \
+    --net vit_large_patch14_reg4_dinov2.lvd142m \
+    --data_zip ...
